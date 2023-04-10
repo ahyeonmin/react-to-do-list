f5585b8a74a7ba30f79df295c903f96c539b4f6e
AHYEON <yotu00@naver.com>
2023-04-10 21:16:37 +0900

    useForm(handleSubmit/formState)

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,16 +1,21 @@
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-    const { register, watch } = useForm();
-    console.log(watch());
+    const { register, watch, handleSubmit, formState } = useForm();
+    const onValid = (data: any) => {
+        console.log(data);
+    };
+    console.log(formState.errors);
     return(
         <div>
-            <form>
-                <input {...register("email")} placeholder="Email"/>
-                <input {...register("firstName")} placeholder="First Name"/>
-                <input {...register("lastName")} placeholder="Last Name"/>
-                <input {...register("phone")} placeholder="Phone"/>
-                <button>Add</button>
+            <form
+                style={{ display: "flex", flexDirection: "column" }}
+                onSubmit={handleSubmit(onValid)}>
+                    <input {...register("email", { required: "Email is required.", minLength: { value: 10, message: "Your email is too short." } })} placeholder="Email"/>
+                    <input {...register("firstName", { required: "First name is required." })} placeholder="First Name"/>
+                    <input {...register("lastName", { required: "Last name is required." })} placeholder="Last Name"/>
+                    <input {...register("phone")} placeholder="Phone"/>
+                    <button>Add</button>
             </form>
         </div>
     );
