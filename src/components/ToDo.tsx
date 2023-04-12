bb5209949cce64be7cfdc968541a0ff58e393e10
AHYEON <yotu00@naver.com>
2023-04-12 12:46:26 +0900

    newToDo (findIndex() / slice())

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,11 +1,17 @@
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text, category }: IToDo) {
+function ToDo({ text, id, category }: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
     const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         const {
             currentTarget: { name }
         } = event;
+        setToDos((oldToDos) => {
+            const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+            const newToDo = { text, id, category: name as any };
+            return [...oldToDos.slice(0, targetIndex), newToDo, ...oldToDos.slice(targetIndex+1)];
+        });
     };
     return (
         <li>
